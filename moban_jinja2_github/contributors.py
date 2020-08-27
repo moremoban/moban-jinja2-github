585690ee1af3dcd6a5259c8bf328e094f79d8ac4
chfw <wangc_2011@hotmail.com>
2020-08-28 00:10:36 +0100

    :bug: handle url not found exception when the github url is not yet ready/does not exist

--- moban_jinja2_github/contributors.py
+++ moban_jinja2_github/contributors.py
@@ -1,9 +1,15 @@
+from gease.exceptions import UrlNotFound
 from gease.contributors import EndPoint
 
 
 def get_contributors(user, repo, author):
     repo = EndPoint(user, repo)
-    user_list = repo.get_all_contributors()
+    try:
+        user_list = repo.get_all_contributors()
 
-    user_list = [detail for detail in user_list if author not in detail["url"]]
-    return user_list
+        user_list = [
+            detail for detail in user_list if author not in detail["url"]
+        ]
+        return user_list
+    except UrlNotFound:
+        return []
