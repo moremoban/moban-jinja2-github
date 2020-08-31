eb2e07b72ce3e26878b760be0f77949bd4a92bc4
chfw <wangc_2011@hotmail.com>
2020-08-31 23:28:20 +0100

    :newsaper: add a new module github issues from moban

--- moban_jinja2_github/__init__.py
+++ moban_jinja2_github/__init__.py
@@ -2,5 +2,7 @@
 from moban_jinja2_github._version import __author__, __version__
 from moban.plugins.jinja2.extensions import jinja_global
 from moban_jinja2_github.contributors import get_contributors
+import moban_jinja2_github.issues
+
 
 jinja_global("moban_jinja2_contributors", get_contributors)
