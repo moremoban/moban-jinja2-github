42a1d43e0d089cb840d339ff7409a100531ecbda
chfw <wangc_2011@hotmail.com>
2020-09-02 23:29:54 +0100

    :handshake: update meta data

--- setup.py
+++ setup.py
@@ -32,14 +32,14 @@ except (ValueError, UnicodeError, locale.Error):
 
 NAME = "moban-jinja2-github"
 AUTHOR = "chfw"
-VERSION = "0.0.2"
+VERSION = "0.0.3"
 EMAIL = "info@moremoban.org"
 LICENSE = "newbsd"
 DESCRIPTION = (
     "Github specific filters, tests and globals"
 )
 URL = "https://github.com/moremoban/moban-jinja2-github"
-DOWNLOAD_URL = "%s/archive/0.0.2.tar.gz" % URL
+DOWNLOAD_URL = "%s/archive/0.0.3.tar.gz" % URL
 FILES = ["README.rst", "CHANGELOG.rst"]
 KEYWORDS = [
     "python",
@@ -71,8 +71,8 @@ EXTRAS_REQUIRE = {
 }
 # You do not need to read beyond this line
 PUBLISH_COMMAND = "{0} setup.py sdist bdist_wheel upload -r pypi".format(sys.executable)
-GS_COMMAND = ("gs moban-jinja2-github v0.0.2 " +
-              "Find 0.0.2 in changelog for more details")
+GS_COMMAND = ("gs moban-jinja2-github v0.0.3 " +
+              "Find 0.0.3 in changelog for more details")
 NO_GS_MESSAGE = ("Automatic github release is disabled. " +
                  "Please install gease to enable it.")
 UPLOAD_FAILED_MSG = (
