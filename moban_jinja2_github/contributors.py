65ab80ab66ab66d5893135f45ea87ccb52cc516b
chfw <wangc_2011@hotmail.com>
2020-10-17 22:40:45 +0100

    :lipstick: update coding styel

--- moban_jinja2_github/contributors.py
+++ moban_jinja2_github/contributors.py
@@ -6,11 +6,12 @@ def get_contributors(user, repo, exclude_contributors=()):
     repo = EndPoint(user, repo)
     try:
         user_list = repo.get_all_contributors()
-
+        print(exclude_contributors)
         user_list = [
             detail
             for detail in user_list
-            if "login" in detail and detail["login"] not in exclude_contributors
+            if "login" in detail
+            and detail["login"] not in exclude_contributors
         ]
         return user_list
     except UrlNotFound:
